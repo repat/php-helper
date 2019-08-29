c5c20eed6b3920186f360ee7083b84ec6d19b37f
repat <repat@repat.de>
2019-08-29 00:01:33 -0700

    Initial Commit

---
+++ src/object_helpers.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!function_exists('object2array')) {
+    /**
+     * Transforms an object into an array
+     *
+     * @param  object|array $stdClassObject
+     * @return array
+     */
+    function object2array($stdClassObject) : array
+    {
+        $array = [];
+        $_array = is_object($stdClassObject) ? get_object_vars($stdClassObject) : $stdClassObject;
+        foreach ($_array as $key => $value) {
+            $value = is_array($value) || is_object($value) ? object2array($value) : $value;
+            $array[$key] = $value;
+        }
+        return $array;
+    }
+}
+
+
+if (!function_exists('filepath2fqcn')) {
+    /**
+     * Filepath to Fully Qualified Class Name
+     *
+     * @param  string $filepath
+     * @param  string $prefix
+     * @return string FQCN of $filepath
+     */
+    function filepath2fqcn(string $filepath, string $prefix = '') : string
+    {
+        if (!empty($prefix)) {
+            $prefix = str_finish($prefix, '/');
+        }
+        return ucfirst(str_replace([$prefix, '/', '.php'], ['', '\\', ''], $filepath));
+    }
+}
---
+++ src/optional_packages_helpers.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!function_exists('markdown2html')
+&& class_exists(\League\CommonMark\CommonMarkConverter::class)) {
+    /**
+     * Converts Markdown text into HTML code with `league/commonmark`
+     *
+     * @param  string $markdown
+     * @return string
+     */
+    function markdown2html(string $markdown) : string
+    {
+        $converter = new \League\CommonMark\CommonMarkConverter;
+        return $converter->convertToHtml($markdown);
+    }
+}
+
+if (!function_exists('domain') &&
+class_exists(\LayerShifter\TLDExtract\Extract::class)) {
+    /**
+     * Gets domain without subdomain etc
+     *
+     * @param  string $url
+     * @return string|null
+     */
+    function domain(string $url) : ?string
+    {
+        $extract = new \LayerShifter\TLDExtract\Extract();
+        $result = $extract->parse($url);
+
+        // If domain parsing worked
+        if (!empty($result->getRegistrableDomain()) && $result->isValidDomain()) {
+            return $result->getRegistrableDomain();
+        }
+
+        return null;
+    }
+}
